78108e4299dfde7353da3af5da3a7dafef0ae664
Sanjarbek <sanjarbekturaxonov@gmail.com>
2021-12-17 16:34:37 +0300

    first commit

---
+++ ss.js
@@ -0,0 +1,7 @@
+var menu = document.querySelector(".menu")
+var navbar = document.querySelector(".navbar")
+var line = document.querySelector(".line")
+var a = 0
+menu.addEventListener("click", function(){
+    navbar.classList.toggle("ochil")
+})
